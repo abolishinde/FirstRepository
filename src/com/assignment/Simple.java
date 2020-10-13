a5e306405e5d4573b7a020b8ab4e9b880577d251
abolishinde <72740286+abolishinde@users.noreply.github.com>
2020-10-13 19:49:26 +0530

    Update Simple.java

--- src/com/assignment/Simple.java
+++ src/com/assignment/Simple.java
@@ -4,7 +4,7 @@ public class Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("I am Aboli Shinde");
+              System.out.println("I am Aboli Shinde,I am from Amravati");
 	}
 
 }
